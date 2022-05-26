8253768141198dbb55b1211954d4cbe5c4b17c17
Jose Velarde <josevelarde00@gmail.com>
2022-05-26 01:08:49 -0300

    CORS fix

--- src/main/java/com/ap/apbackend/Controller/EducationController.java
+++ src/main/java/com/ap/apbackend/Controller/EducationController.java
@@ -15,6 +15,7 @@ import com.ap.apbackend.Service.EducationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class EducationController {
   EducationService educationService;
--- src/main/java/com/ap/apbackend/Controller/ExperienceController.java
+++ src/main/java/com/ap/apbackend/Controller/ExperienceController.java
@@ -10,6 +10,7 @@ import com.ap.apbackend.Model.Experience;
 import com.ap.apbackend.Service.ExperienceService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ExperienceController {
   ExperienceService experienceService;
