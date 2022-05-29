dace34e99e97733806b733d0cd14ef09f256133c
Jose Velarde <josevelarde00@gmail.com>
2022-05-29 19:36:41 -0300

    Delete issue

--- src/main/java/com/ap/apbackend/Model/Skills.java
+++ src/main/java/com/ap/apbackend/Model/Skills.java
@@ -35,11 +35,11 @@ public class Skills {
   @Column(name = "skills_id")
   private Long id;
   @ElementCollection(fetch = FetchType.LAZY)
-  @CollectionTable(name = "soft_skills", joinColumns = @JoinColumn(name = "skills_id"))
+  @CollectionTable(name = "soft_skills", joinColumns = @JoinColumn(name = "soft_skills_id"))
   @OrderColumn
   private List<Skill> soft_skills;
   @ElementCollection(fetch = FetchType.LAZY)
-  @CollectionTable(name = "hard_skills", joinColumns = @JoinColumn(name = "skills_id"))
+  @CollectionTable(name = "hard_skills", joinColumns = @JoinColumn(name = "hard_skills_id"))
   @OrderColumn
   private List<Skill> hard_skills;
 
