8d03296812189c133a2b32358aa954cdf395a278
Jose Velarde <josevelarde00@gmail.com>
2022-05-29 19:22:55 -0300

    Fix

--- src/main/java/com/ap/apbackend/Model/Skills.java
+++ src/main/java/com/ap/apbackend/Model/Skills.java
@@ -17,7 +17,6 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import org.hibernate.annotations.IndexColumn;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
@@ -34,11 +33,11 @@ public class Skills {
   @Column(name = "skills_id")
   private Long id;
   @ElementCollection(fetch = FetchType.LAZY)
-  @CollectionTable(name = "soft_skills", joinColumns = @JoinColumn(name = "skills"))
+  @CollectionTable(name = "soft_skills", joinColumns = @JoinColumn(name = "soft_skills_id"))
   @OrderColumn
   private List<Skill> soft_skills;
   @ElementCollection(fetch = FetchType.LAZY)
-  @CollectionTable(name = "hard_skills", joinColumns = @JoinColumn(name = "skills"))
+  @CollectionTable(name = "hard_skills", joinColumns = @JoinColumn(name = "hard_skills_id"))
   @OrderColumn
   private List<Skill> hard_skills;
 
