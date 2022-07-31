d3b304aa311772f7fc93c0bc078371d0c5896307
Jose Velarde <josevelarde00@gmail.com>
2022-07-31 00:25:00 -0300

    cors

--- src/main/java/com/ap/apbackend/ApBackendApplication.java
+++ src/main/java/com/ap/apbackend/ApBackendApplication.java
@@ -18,7 +18,7 @@ public class ApBackendApplication {
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api").allowedOrigins("http://localhost:4200");
+        registry.addMapping("/api/**").allowedOrigins("http://localhost:4200");
       }
     };
   }
