11206076996b062e575cba7cb9c214e07d1d1685
Jose Velarde <josevelarde00@gmail.com>
2022-11-19 19:20:20 -0300

    Swagger

--- src/main/java/com/ap/apbackend/Swagger2/SwaggerConfig.java
+++ src/main/java/com/ap/apbackend/Swagger2/SwaggerConfig.java
@@ -14,8 +14,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
   @Bean
   public Docket api() {
-    return new Docket(DocumentationType.SWAGGER_2).select()
-        .apis(RequestHandlerSelectors.basePackage("com.ap.apbackend")).paths(PathSelectors.any()).build();
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any())
+        .build();
   }
 
 }
